55b5a9d9441f73321e232d21052b290664529be0
Isaque Franklin Amaral <isaquefrankli@gmail.com>
2024-03-02 17:13:59 -0300

    first commit

---
+++ main.go
@@ -0,0 +1,160 @@
+package main
+
+//Those are just the imports I'm going to need for this blockchain project.
+import (
+	"crypto/sha256"
+	"encoding/hex"
+	"encoding/json"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"time"
+  	"fmt"
+
+	"github.com/davecgh/go-spew/spew"
+	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
+)
+
+
+//This is the struct for each of the blocks in the blockchain.
+
+type Block struct {
+	Index      int
+	Timestamp  string
+	BPM        int
+	Hash       string
+	PrevHash   string
+}
+
+//The block is the data that will be written to the blockchain.
+//Index is the position of data record in the blockchain (block height)
+//Timestamp is the time the data is written to the blockchain.
+//BPM or beats per minute is my pulse rate.
+//Hash is a Sha256 indentifier representing the data record.
+//PrevHash is the Sha256 identifier of the previous block.
+
+var Blockchain []Block
+//The blockchain will simply be a slice type Block(struct).
+
+//That's the function to create a hash.
+func calculateHash(block Block) string {
+	record := string(block.Index) + block.Timestamp + string(block.BPM) + block.PrevHash
+	h := sha256.New()
+	h.Write([]byte(record))
+	hashed := h.sum(nil)
+	return hex.EncodeToString(hashed)
+}
+
+//Thats the function to generate a new Block based on the data of the previuous block. 
+func generateBlock(oldBlock Block, BPM int) (Block, error) {
+	var newBlock Block
+
+	t := time.Now()
+
+	newBlock.Index = oldBlock.Index + 1
+	newBlock.Timestamp = t.string()
+	newBlock.BPM = BPM
+	newBlock.PrevHash = oldBlock.hash
+	newBlock.hash = calculateHash(newBlock)
+
+	return newBlock, nil
+}
+
+//Block validation => Functions to make sure the blocks haven't been tempered with.
+func isBlockValid(newBlock, oldBlock Block) bool {
+	if oldBlock.Index + 1 != newBlock.Index {
+		return false
+	}
+
+	if oldBlock.Hash != newBlock.PrevHash {
+		return false
+	}
+
+	if calculateHash(newBlock) != newBlock.Hash {
+		return false
+	}
+
+	return true
+}
+
+//A function to choose between two blockchains with different lengths in different nodes.
+func replaceChain(newBlocks []Block) {
+	if len(newBlocks) > len(Blockchain) {
+		Blockchain = newBlocks
+	}
+}
+
+//This is the web server and this function will be called later.
+func run() error {
+	mux := makeMuxRouter()
+	httpAddr := os.Getenv("ADDR")
+	log.Println("Listening on ", os.Getenv("ADDR"))
+	s := &http.Server {
+		addr:           ":" + httpAddr,
+		Handler:        mux,
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+
+	if err := s.ListenAndServe(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+//Our routes to get and post to the blockchain
+func makeMuxRouter() http.Handler {
+	muxRouter := mux.NewRouter()
+	muxRouter.HandleFunc("/", handleGetBlockchain).Methods("GET")
+	muxRouter.HandleFunc("/", handleWriteBlock).Methods("POST")
+	return muxRouter
+}
+
+//Function for the GET handler in server
+func handleGetBlockchain(w http.ResponseWriter, r *http.Request) {
+	bytes, err := json.MarshalIndent(Blockchain, "", " ")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	io.WriteString(w, string(bytes))
+}
+
+//Starting the procediment for the POST func.
+type Message struct {
+	BPM int
+}
+
+func handleWriteBlock(w http.ResponseWriter, r *http.Request) {
+	var m Message
+
+	decoder := json.NewDecoder(r.Body)
+	if err := decoder.Decode(&m); err != nil {
+		respondWithJSON(w, r, http.StatusBadResquest, r.Body)
+		return
+	}
+
+	defer r.Body.Close()
+
+	newBlock, err := generateBlock(Blockchain[len(blockchain)-1], m.BPM)
+	if err != nil {
+		respondWithJSON(w, r, http.StatusInternalServerError, m)
+		return
+	}
+	if isBlockValid(newBlock, Blockchain[len(Blockchain)-1]) {
+		newBlochchain := append(Blockchain, newBlock)
+		replaceChain(newBlochchain)
+		spew.Dump(Blockchain)
+	}
+
+	respondWithJSON(w, r, http.StatusCreated, newBlock)
+}
+
+func main(){
+  fmt.Println("Hello world!")
+  fmt.Println("I am a truly evil genius!")
+}
